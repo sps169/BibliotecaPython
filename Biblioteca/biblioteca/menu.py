778a0092a09f3cb501b83ead914e3031177b256a
Sergio <sps169@outlook.es>
2022-02-17 01:24:11 +0100

    created biblioteca

--- Biblioteca/biblioteca/menu.py
+++ Biblioteca/biblioteca/menu.py
@@ -19,3 +19,4 @@ def get_user_input() :
 def options_switch(user_input) :
     if (user_input == 1) :
         add_usuario()
+    
--- Biblioteca/biblioteca/user_handler.py
+++
@@ -1,7 +0,0 @@
-from Biblioteca.biblioteca.model import Usuario
-
-
-def add_usuario () :
-    usuario = Usuario()
-    #recibir datos usuario a travs de input
-    return usuario
